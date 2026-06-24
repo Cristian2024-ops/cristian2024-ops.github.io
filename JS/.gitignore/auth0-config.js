064c8d489909c8878be85cc976dc7194298db448
Cristian2024-ops <cristian.jhon.aguilar35a@gmail.com>
2026-06-24 01:49:21 -0400

    Delete JS/.gitignore directory

--- JS/.gitignore/auth0-config.js
+++
@@ -1,4 +0,0 @@
-window.AUTH0_DOMAIN = 'dev-ekkbx30j1ns6gm5g.us.auth0.com';
-window.AUTH0_CLIENT_ID = 'Qlsh3WVqus5Hwwl4nWp96Uq0yo8gbUnC';
-window.AUTH0_REDIRECT_URI = 'https://cristian2024-ops.github.io/login.html';
-window.AUTH0_LOGOUT_REDIRECT_URI = 'https://cristian2024-ops.github.io/';
